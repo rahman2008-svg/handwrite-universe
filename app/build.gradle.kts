c4c3238debe7a63e601293fddbc276cc6f1f3042
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-07-18 14:15:46 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.handwriteuniverse.qwyztr"
+    applicationId = "nexvora.handwriteuniverse.qwyztr"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
